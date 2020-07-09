2872c9f96362743b2167262efe0d28069bc8e794
Gleb <azrieldevpoper@gmail.com>
2020-07-09 23:12:37 +0300

    linked the vault to the todo list

--- src/app/components/todo-for-beer/todo-for-beer.component.ts
+++ src/app/components/todo-for-beer/todo-for-beer.component.ts
@@ -9,34 +9,59 @@ import { HttpClient } from '@angular/common/http';
 export class TodoForBeerComponent implements OnInit {
   response: any;
   todos: object[] = [];
-  valueInput: string;
+  intermediateTodos: Todos;
+  beers: any;
 
   constructor(private http: HttpClient) {}
+
+  refreshBeers(beerID: number, thisTodos: any) {
+    this.beers = this.beers.filter((beer: any) => beer.id != beerID);
+
+    this.beers.push({
+      id: beerID,
+      todos: thisTodos,
+    });
+
+    localStorage.setItem('beers', JSON.stringify(this.beers));
+  }
 
   ngOnInit(): void {
     this.http.get('https://api.punkapi.com/v2/beers').subscribe((response) => {
       this.response = response;
-      console.log(this.response);
     });
-    
+
     this.todos = JSON.parse(localStorage.getItem('beers')) || [];
+
+    this.beers = JSON.parse(localStorage.getItem('beers')) || [];
   }
 
-  addTodo() {
-    if (this.valueInput) {
-      this.todos.push({
-        info: this.valueInput,
-        done: false,
-      });
-    }
-    this.valueInput = '';
-  }
-
-  doneTodo(todo: any) {
+  doneTodo(todo: any, item: any, id: number) {
     todo.done = !todo.done;
+
+    localStorage.setItem('beer_' + id, JSON.stringify(item));
+
+    this.refreshBeers(id, item);
   }
 
-  removeTodo(todo: object) {
-    this.todos = this.todos.filter((_todo) => _todo != todo);
+  removeTodo(todo: any, item: any, id: number) {
+    this.todos = this.todos.filter((_todo:any) =>_todo.id != id);
+
+    item = item.filter((_todo:any)=> _todo != todo);
+
+    this.intermediateTodos = {  
+      id: id,
+      todos: item,
+    }    
+
+    this.todos.push(this.intermediateTodos);
+
+    localStorage.setItem('beer_' + id, JSON.stringify(item));
+
+    this.refreshBeers(id, item);
   }
+}
+
+export interface Todos{
+  id:number,
+  todos:object[],
 }
